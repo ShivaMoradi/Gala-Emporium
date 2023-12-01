d2222e4e45b54b1c5fb0cb32464a332616ac7d9c
cesu1 <celestesun@hotmail.com>
2023-12-01 21:14:19 +0100

    tried to adjust first row in clubpages

--- client/pages/clubpages.js
+++ client/pages/clubpages.js
@@ -1,11 +1,11 @@
 function displayImage() {
-    const imageUrl = './images/blue club jazz.jpg'
+  const imageUrl = './images/blue club jazz.jpg'
 
-    const imageElement = document.createElement('img')
-    imageElement.src = imageUrl
-    imageElement.alt = 'Blue Club Jazz'
-    const imageContainer = document.getElementById('image-container');
-    imageContainer.appendChild(imageElement)
+  const imageElement = document.createElement('img')
+  imageElement.src = imageUrl
+  imageElement.alt = 'Blue Club Jazz'
+  const imageContainer = document.getElementById('image-container');
+  imageContainer.appendChild(imageElement)
 }
 
 displayImage();
