bc974b108dc91612113ab9cdaf8c1ad766326633
cesu1 <celestesun@hotmail.com>
2023-12-01 10:41:39 +0100

    lagt till klubbar och nav.länkar

---
+++ client/pages/clubpages.js
@@ -0,0 +1,17 @@
+const clubs = [
+  { name: "Blue Club", id: "blue-club" },
+  { name: "Rock Club", id: "rock-club" },
+];
+
+// navigationslänkar för varje klubb
+function generateClubNavigation() {
+  let navigationHTML = '';
+  clubs.forEach(club => {
+    navigationHTML += `<a href="#${club.id}" class="club-link">${club.name}</a> `;
+  });
+  document.getElementById('club-navigation').innerHTML = navigationHTML;
+}
+
+$(document).ready(function () {
+  generateClubNavigation();
+});
