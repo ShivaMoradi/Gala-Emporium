7f10dbb972a4d75589ff2f433dfa0fad4b299b6a
desmonddibba <Desmonddibba@gmail.com>
2023-12-05 16:12:38 +0100

    Created basic structure for clubpages

--- client/pages/clubpages.js
+++ client/pages/clubpages.js
@@ -1,49 +1,77 @@
-function displayImage() {
-  const imageUrl = '../images/coverimg.jpg'; // Hårdkodad sökväg till cover-bild
+export default function clubpages(){
+  return `
+  <div id="clubPage">
+  <header>
+      <h1>Club title</h1>
+  </header>
 
-  const imageElement = document.createElement('img');
-  imageElement.src = imageUrl;
-  imageElement.alt = altText;
-  const imageContainer = document.getElementById('image-container');
-  imageContainer.innerHTML = ''; 
-  imageContainer.appendChild(imageElement);
-}
-
-displayImage();
-
-
-const clubs = [
-  { name: "Blue Club", id: "blue-club", description: "The main description of the Blue Club...", imageUrl: '../images/blue club jazz.jpg' },
-  { name: "Rock Club", id: "rock-club", description: "The main description of the Rock Club...", imageUrl: '../images/rock-club-rock.jpg' },
-  // Lägg till fler klubbar här...
-];
-
-
-function clubpages() {
-  let clubsHTML = clubs.map(club => {
-    return `
-      <div class='club'>
-        <h3>${club.name}</h3>
-        <p>${club.description}</p>
-        <button class="show-events-btn" data-club="${club.name}">Show Events</button>
+  <section id="clubBody">
+      <div id="mediaContainer"> 
+          <p> Video and images goes here</p>       
+          <!---VIDEO AND IMAGES GO HERE-->
       </div>
-    `;
-  }).join('');
-  $('#club-list').html(clubsHTML);
+      
+      <article id="clubDescription">
+          <p>Club Description goes here</p>
+          <!-- CLUB DESRIPTION GOES HERE  -->
+      </article>
+
+      <section id="event-list">
+          <p>Club specific events goes here</p>
+          <!---EVENTS GOES HERE-->
+      </section>
+  </section>
+</div>
+  `
 }
 
-$(document).on('click', '.show-events-btn', function () {
-  const clubName = $(this).data('club');
-  const selectedClub = clubs.find(club => club.name === clubName);
-  if (selectedClub) {
-    displayImage(selectedClub.imageUrl, selectedClub.name); // Bild på klubben
-    displayEventsForClub(clubName);
-  }
-});
+
+// function displayImage() {
+//   const imageUrl = '../images/coverimg.jpg'; // Hårdkodad sökväg till cover-bild
+
+//   const imageElement = document.createElement('img');
+//   imageElement.src = imageUrl;
+//   imageElement.alt = altText;
+//   const imageContainer = document.getElementById('image-container');
+//   imageContainer.innerHTML = ''; 
+//   imageContainer.appendChild(imageElement);
+// }
+
+// displayImage();
 
 
-// Exportera clubpages, vi använder ES6-moduler?
-export { clubpages };
+// const clubs = [
+//   { name: "Blue Club", id: "blue-club", description: "The main description of the Blue Club...", imageUrl: '../images/blue club jazz.jpg' },
+//   { name: "Rock Club", id: "rock-club", description: "The main description of the Rock Club...", imageUrl: '../images/rock-club-rock.jpg' },
+//   // Lägg till fler klubbar här...
+// ];
+
+
+// function clubpages() {
+//   let clubsHTML = clubs.map(club => {
+//     return `
+//       <div class='club'>
+//         <h3>${club.name}</h3>
+//         <p>${club.description}</p>
+//         <button class="show-events-btn" data-club="${club.name}">Show Events</button>
+//       </div>
+//     `;
+//   }).join('');
+//   $('#club-list').html(clubsHTML);
+// }
+
+// $(document).on('click', '.show-events-btn', function () {
+//   const clubName = $(this).data('club');
+//   const selectedClub = clubs.find(club => club.name === clubName);
+//   if (selectedClub) {
+//     displayImage(selectedClub.imageUrl, selectedClub.name); // Bild på klubben
+//     displayEventsForClub(clubName);
+//   }
+// });
+
+
+// // Exportera clubpages, vi använder ES6-moduler?
+// export { clubpages };
 
   
   
