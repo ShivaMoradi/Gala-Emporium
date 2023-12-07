8e486517de42c1f68ce7add7dfa780a308d64497
desmonddibba <desmonddibba@gmail.com>
2023-12-07 15:03:06 +0100

    Changes to db variable names

--- server/api/club.js
+++ server/api/club.js
@@ -1,19 +1,20 @@
 export default function (server, db) {
-
+ 
   server.get('/api/club', async (req, res) => {
-    const club = await db.query("SELECT * FROM event_club ")
+    const club = await db.query("SELECT * FROM clubEvents ")
     console.log(club)
     res.json(club)
   })
-  server.get('/api/club/:clubName', async (req, res) => {//clubName 
-    const club = await db.query("SELECT * FROM event_club WHERE club_name = ?", [req.params.clubName])
+  
+  server.get('/api/club/:clubName', async (req, res) => {//clubName
+    const club = await db.query("SELECT * FROM clubEvents WHERE clubName = ?", [req.params.clubName])
     console.log(club)
     res.json(club)
   })
-
+ 
   server.post('/api/club', async (req, res) => {
     if (req.body.name.trim().length > 0) {
-      const result = await db.query("INSERT INTO event_club (event_name, event_description, event_date, event_address, event_price, club_name, club_description) VALUES (?, ?, ?, ?, ?, ?, ?)", [req.body.event_name, req.body.event_description, req.body.event_date, req.body.event_address, req.body.event_price, req.body.club_name, req.body.club_description])
+      const result = await db.query("INSERT INTO clubEvents (eventName, eventDescription, date, address, price, clubName, clubDescription) VALUES (?, ?, ?, ?, ?, ?, ?)", [req.body.eventName, req.body.eventDescription, req.body.date, req.body.address, req.body.price, req.body.clubName, req.body.clubDescription])
       result.clubAdded = true
       res.json(result)
       console.log("Result - ", result);
@@ -22,11 +23,11 @@ export default function (server, db) {
       res.json({ clubAdded: false })
     }
   })
-
+ 
   server.put('/api/club/:id', (req, res) => {
   })
-
+ 
   server.delete('/api/club/:id', (req, res) => {
   })
-
+ 
 }
