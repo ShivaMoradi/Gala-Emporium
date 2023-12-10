dfdd07a400b6a7c7894e5535f8e88ad0d1c58346
cesu1 <celestesun@hotmail.com>
2023-12-10 21:14:08 +0100

    lagt events på homepage och add events på addevents

--- client/pages/addevents.js
+++ client/pages/addevents.js
@@ -1,9 +1,10 @@
-export {getEventsForClub, createEventHTML};
+
+export {getEventsForClub, createEventHTML, event};
 
 
 // Filter Events based on 
     function getEventsForClub(clubName, clubData){
-        return clubData.filter(event =>  clubName.toLowerCase() ===  event.clubName.toLowerCase())
+        return clubData.filter(event => clubName.toLowerCase() ===  event.clubName.toLowerCase())
     }
 
 
@@ -12,19 +13,80 @@ export {getEventsForClub, createEventHTML};
 //Create HTML structure to display events.
 //TODO Remove image style when css has been applied to event-image.
 //TODO Create link to event webpage.
-    function createEventHTML(clubData){
-        return `
-            <div class='event'>
-                <h3> <a href="#" class="event-title-link" data-id="${clubData.id}">${clubData.eventName}</a></h3> 
-                <p>${clubData.clubName}</p>
-                <img src="${clubData.image}" alt="Image of ${clubData.eventName}" class="event-image" style="max-width:300px;height:auto;">  
-                <p>Date: ${clubData.date}</p>
-                <p>Time: ${clubData.time}</p>
-                <p>Description: ${clubData.eventDescription}</p>
-                <p>Price: ${clubData.price}</p>
-            </div>    
-        `
-    };
+// Fetch club data from the backend
+function createEventHTML(clubData) {
+    return `
+        <div class='event'>
+            <h3><a href="#" class="event-title-link" data-id="${clubData.id}">${clubData.eventName}</a></h3>
+            <p>${clubData.clubName}</p>
+            <img src="${clubData.image}" alt="Image of ${clubData.eventName}" class="event-image">
+            <p>Date: ${clubData.date}</p>
+            <p>Description: ${clubData.eventDescription}</p>
+            <p>Price: ${clubData.price}</p>
+        </div>
+    `;
+}
+
+        // Fetch club data from the backend
+        fetch( '/api/club' )
+            .then( response => response.json() )
+            .then( data => {
+                const eventDisplay = document.getElementById( 'eventDisplay' );
+
+                data.forEach( clubData => {
+                    const eventHTML = createEventHTML( clubData );
+                    const eventContainer = document.createElement( 'div' );
+                    eventContainer.innerHTML = eventHTML;
+                    eventContainer.classList.add( 'event' );
+                
+                    eventDisplay.appendChild( eventContainer );
+                } );
+            } )
+            .catch( error => {
+                console.error( 'Error fetching club data:', error );
+            } );
+
+function event() {
+    return `
+  <form class= "addEvent" onsubmit="addEvent(); return false">
+    <h1>Add a new event here: </h1>
+    <input type="varchar(200)" name="eventsName" placeholder="events name">
+        <input type="varchar(255)" eventDescription="eventsDescription" placeholder="events description">
+    <input type="datetime" data="eventsDate" placeholder="events date">
+    <input type="varchar(200)" address="eventsAddress" placeholder="events address">
+    <input type="int" price="eventsPrice" placeholder="events price">
+    <input type="int" clubId="clubId" placeholder="club id">
+    <input type="blob" name="eventsImages" placeholder="events images">
+    <input id="submit" type="submit" value="Add Event">
+    
+
+  </form>
+  `
+}
+
+async function addEvent () {
+    const eventName = $( "[name=eventName]" ).val()
+    console.log( eventName )
+
+    if ( eventName.trim().length > 0 ) {
+        const response = await fetch( "api/club", {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify( { name: eventName } )
+        } )
+        const result = await response.json()
+
+        if ( result.eventAdded ) {
+            alert( `${ eventName.trim() } added` )
+            $( "[name=eventName]" ).val( "" )
+        }
+    } else {
+        alert( "Please write your event name!" )
+    }
+}
+event();
+
+window.addBook = addEvent
 
 
 // Create HTML Structure in different div to style differently. (full-page event view?)
--- client/router.js
+++ client/router.js
@@ -1,24 +1,29 @@
 import login from "./pages/login.js";
 $('#login').html(login());
 
+import { createEventHTML, event } from "./pages/addevents.js";
 import clubPages from "./pages/clubpages.js";
-import bookClub from "./pages/clubpages.js";
+
 
 async function router() {
-let content;
+  let content;
   // Populate "content" with whatever
-switch(window.location.hash){
+  switch (window.location.hash) {
     case "":
-      content = "<h1> Homepage </h1>"
+      content = await getAllEvents();
       break;
-    case "#clubpages":
-    content = await clubPages("blue club");
-    break;
+
+    case "#rockClub":
+      content = await clubPages("rock club");
+      break;
+    case "#blueClub":
+      content = await clubPages("blue club");
+      break;
     case "#bookClub":
-    content = await clubPages("book club");
-    break;
-    case "addevent":
-      content = "<h1>Placeholder for addevent page</h1>"
+      content = await clubPages("book club");
+      break;
+    case "#addevent":
+      content = await event("");
       break;
     default:
       content = "<h1><bold>Page not found!</bold></h1>"
@@ -26,9 +31,16 @@ switch(window.location.hash){
   }
 
   // Populate <main> with whatever content.
- $('main').html(content);
+  $('main').html(content);
 
+}
+
+async function getAllEvents() {
+  const data = await fetch('/api/club').then(response => response.json());
+  const eventsHTML = data.map(createEventHTML).join('');
+  return `<div class="grid-container">${eventsHTML}</div>`;
 }
 
 window.onload = router
 window.onhashchange = router
+
