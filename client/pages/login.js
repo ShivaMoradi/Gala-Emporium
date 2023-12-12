072d484763da5b2fb29c6411a6b8fd7b5e94a73e
ShivaMoradi <shiva.moradi1@gmail.com>
2023-12-12 12:01:24 +0100

    added some function in login.js client

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,4 +1,4 @@
-export default function () {
+/*export default function () {
   return `
     <form>
       <input name="email" placeholder="your email">
@@ -6,4 +6,72 @@ export default function () {
       <input type="submit" value="Login">
     </form>  
   `
+}*/
+
+
+export default function init () {
+  return `
+    <form onsubmit="login(); return false">
+      <input name="email" placeholder="your email">
+      <input name="password" placeholder="your password">
+      <input type="submit" value="Login">
+    </form>  
+  `
 }
+
+async function login () {
+  const credentials = {
+    email: $( '[name=email]' ).val(),
+    password: $( '[name=password]' ).val()
+  }
+  console.log( credentials )
+  let response = await fetch( '/api/login', {
+    // tell the server we want to send/create data
+    method: 'post',
+    // and that we will send data json formatted
+    headers: { 'Content-Type': 'application/json' },
+    // the data encoded as json
+    body: JSON.stringify( credentials )
+  } );
+  let result = await response.json();
+  console.log( result )
+  if ( result.loggedIn ) {
+    $( '#login' ).html( `
+      <button onclick="logout()">Logout</button>
+    `)
+    cart() // render cart when you login
+  }
+
+}
+
+window.login = login // expose login to global (html) scope
+
+
+async function logout () {
+  console.log( 'sir, logging out?' )
+  let response = await fetch( '/api/login', {
+    method: 'delete'
+  } );
+  let result = await response.json();
+  console.log( result )
+  if ( !result.loggedIn ) {
+    $( '#login' ).html( init() )
+    $( "#addevent" ).html( "" ) // remove add event when you logout
+  }
+}
+
+window.logout = logout
+
+
+async function checkLogin () {
+  const response = await fetch( '/api/login' )
+  const result = await response.json()
+  console.log( result )
+  if ( result.loggedIn || result.email ) {
+    $( '#login' ).html( `
+      <button onclick="logout()">Logout</button>
+    `)
+  }
+}
+
+checkLogin() // will execute on load
