4f1fa5ceef523d972bb03c10c96ec5fae7b5b6e2
ti3ktock <laura.tailor@gmail.com>
2023-12-13 22:24:36 +0100

    login function responds now

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,70 +1,38 @@
-import { event, addEvent } from "./addevents.js";
-export default async function init() {
-  return `
-    <form onsubmit="login(); return false">
-      <input name="email" placeholder="your email">
-      <input name="password" placeholder="your password">
-      <input type="submit" value="Login">
-    </form>  
-  `
-}
-
 async function login() {
+  const email = $('[name=email]').val();
+  const password = $('[name=password]').val();
+
+  // Check if both email and password are provided
+  if (!email || !password) {
+    console.log('Please provide both email and password.');
+    return;
+  }
   const credentials = {
     email: $('[name=email]').val(),
     password: $('[name=password]').val()
   }
   $("#addEventbyAdmin").html(await event())
   console.log(credentials)
-  let response = await fetch('/api/login', {
-    // tell the server we want to send/create data
-    method: 'post',
-    // and that we will send data json formatted
-    headers: { 'Content-Type': 'application/json' },
-    // the data encoded as json
-    body: JSON.stringify(credentials)
-  });
-  let result = await response.json();
-  console.log(result)
-  if (result.loggedIn) {
-    $('#login').html(`
+
+  try {
+    let response = await fetch('/api/login', {
+      // tell the server we want to send/create data
+      method: 'post',
+      // and that we will send data json formatted
+      headers: { 'Content-Type': 'application/json' },
+      // the data encoded as json
+      body: JSON.stringify(credentials)
+    });
+    let result = await response.json();
+    console.log(result)
+    if (result.loggedIn) {
+      $('#login').html(`
       <button onclick="logout()">Logout</button>
     `)
-    event()// render event when you login
+      event()// render event when you login
 
+    }
+  } catch (error) {
+    console.error('Error during login:', error);
   }
 }
-
-
-
-window.login = login
-
-
-async function logout() {
-  console.log('sir, logging out?')
-  let response = await fetch('/api/login', {
-    method: 'delete'
-  });
-  let result = await response.json();
-  console.log(result)
-  if (!result.loggedIn) {
-    $('#login').html(await init())
-  }
-}
-
-window.logout = logout
-
-
-async function checkLogin() {
-  const response = await fetch('/api/login')
-  const result = await response.json()
-  console.log(result)
-  if (result.loggedIn || result.email) {
-    $('#login').html(`
-      <button onclick="logout()">Logout</button>
-    `)
-
-  }
-}
-
-checkLogin() // will execute on load
