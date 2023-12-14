e274c13432bee65bd9a53b4fdf96035643f5245f
ti3ktock <laura.tailor@gmail.com>
2023-12-14 21:17:34 +0100

    book club is changed to book in the cases

--- client/router.js
+++ client/router.js
@@ -21,7 +21,7 @@ async function router() {
       content = await clubPages("blue club");
       break;
     case "#bookClub":
-      content = await clubPages("book club");
+      content = await clubPages("book");
       break;
     case "#jumpingClub":
       content = await clubPages("jumping club");
