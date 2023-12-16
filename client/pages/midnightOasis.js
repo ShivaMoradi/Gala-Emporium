be5651ff97bce0884db3e490bcbbfe862011a346
desmonddibba <desmonddibba@gmail.com>
2023-12-16 18:42:11 +0100

    wrote functions to fetch clubdata and eventdata seperately, in case there are no events, club data must still be generated from db

--- client/pages/midnightOasis.js
+++ client/pages/midnightOasis.js
@@ -1,28 +1,62 @@
 export default async function midnightOasis(){
+    const clubInfo = await fetchClubInfo();
+    const eventInfo = await fetchEventInfo();
+    
+    if(!clubInfo || clubInfo.length === 0){
+        return `<p> There was an error returning club data for Midnight Oasis </p> `
+    }
 
 
+    const clubDetails = clubInfo[0]
+    console.log(clubInfo);
+    console.log(clubDetails);
+    console.log(eventInfo);
+
+
+
+
+    // Generate the events HTML if there are any events
+    let eventsHtml = '<p>No upcoming events.</p>';
+
+    // Construct the HTML for the club details and events
     return `
-    <div id="midnightOasisBody"> 
+        <div id="midnightOasisBody"> 
             <header>
                 <div id="titleBackdrop">
-                    <h1>Midnight Oasis</h1>
+                    <h1>${clubDetails.name}</h1>
                 </div>
             </header>
             
             <main>
-            <!---Main Content of Club-->
                 <section>
                     <h2>About</h2>
-                    <p>Some description</p>
+                    <p>${clubDetails.description}</p>
                 </section>
                 
                 <section>
-                    <h1>Events</h1>
+                    <h2>Events</h2>
+                    ${eventsHtml}
                 </section>
-
             </main>
-    </div>
-    `
+        </div>
+    `;
+
+}
 
 
+
+
+
+
+async function fetchClubInfo(){
+    const response = await fetch("/api/club/details/Midnight Oasis");
+    const clubData = await response.json();
+    return clubData;
+}
+
+
+async function fetchEventInfo(){
+    const response = await fetch("/api/club/Midnight Oasis");
+    const clubData = await response.json();
+    return clubData;
 }
