40b2ce89ce686e6d359cd1551d3086967abb27ab
desmonddibba <desmonddibba@gmail.com>
2023-12-16 17:33:40 +0100

    renamed blueClub to midnightOasis everywhere

---
+++ client/pages/midnightOasis.js
@@ -0,0 +1,28 @@
+export default async function midnightOasis(){
+
+
+    return `
+    <div id="midnightOasisBody"> 
+            <header>
+                <div id="titleBackdrop">
+                    <h1>Midnight Oasis</h1>
+                </div>
+            </header>
+            
+            <main>
+            <!---Main Content of Club-->
+                <section>
+                    <h2>About</h2>
+                    <p>Some description</p>
+                </section>
+                
+                <section>
+                    <h1>Events</h1>
+                </section>
+
+            </main>
+    </div>
+    `
+
+
+}
