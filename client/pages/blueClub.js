d768b7657a89f09cc25f599831dc6d39cb7e21dd
desmonddibba <desmonddibba@gmail.com>
2023-12-16 17:17:26 +0100

    Added basic styling and html structure to BlueClub.js and midnightOasisStyle.css

--- client/pages/blueClub.js
+++ client/pages/blueClub.js
@@ -2,7 +2,28 @@ export default async function blueClub(){
 
 
     return `
-        <p> Blue Club <p>    
+
+
+    <div id="midnightOasisBody"> 
+            <header>
+                <div id="titleBackdrop">
+                    <h1>Midnight Oasis</h1>
+                </div>
+            </header>
+            
+            <main>
+            <!---Main Content of Club-->
+                <section>
+                    <h2>About</h2>
+                    <p>Some description</p>
+                </section>
+                
+                <section>
+                    <h1>Events</h1>
+                </section>
+
+            </main>
+    </div>
     `
 
 
